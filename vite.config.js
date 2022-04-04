62808bbe80822cda6e03cefae3218f41fd0456a0
cbdyzj <cbdyzj@jianzhao.org>
2022-04-04 22:02:47 +0800

    Update Chore

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    root: './src',
     build: {
-        outDir: '../dist',
-        emptyOutDir: true,
         rollupOptions: {
             input: [
-                './src/index.html',
+                './index.html',
                 './src/css/index.html',
                 './src/figma/index.html',
                 './src/rAF/index.html',
