93f2415af064a02b11000fb7d4a3c51078093dca
cbdyzj <cbdyzj@jianzhao.org>
2023-08-14 23:11:48 +0800

    Amend

--- public/lu2_pack.js
+++ public/lu2_pack.js
@@ -3,4 +3,4 @@ import lu2_ui from 'https://unpkg.com/lu2@2023.8.5/theme/edge/css/common/ui.min.
 import 'https://unpkg.com/lu2@2023.8.5/theme/edge/js/common/safari-polyfill.min.js'
 import 'https://unpkg.com/lu2@2023.8.5/theme/edge/js/common/all.min.js'
 
-document.adoptedStyleSheets = [lu2_animate, lu2_ui]
+document.adoptedStyleSheets = [...document.adoptedStyleSheets, lu2_animate, lu2_ui]
